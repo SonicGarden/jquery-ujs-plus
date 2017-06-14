f4e780134261223736652605d8fc457e387f1098
aki <lala.akira@gmail.com>
2017-06-14 23:20:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# jquery-ujs plus
+
+## Using
+
+```
+yarn install jquery-ujs-plus
+```
+
+```javascript
+import 'jquery-ujs-plus'
+```
---
+++ src/rails.js
@@ -0,0 +1,53 @@
+// eslint-disable-next-line func-names
+(function ($) {
+  const { allowAction } = $.rails;
+  const origAllowAction = allowAction.bind($.rails);
+
+  // NOTE: HTML5のvalidation でエラーのある場合に confirm ダイアログを表示するとネイティブのエラーメッセージが表示されない動作の改善
+  // https://github.com/rails/jquery-ujs/blob/3bff9e049afcb38191c6eb470062b9e51f46e535/src/rails.js#L294
+  // eslint-disable-next-line no-param-reassign
+  $.rails.allowAction = (element) => {
+    const form = element.closest('form').get(0);
+    if (form && form.checkValidity && !form.checkValidity()) {
+      return true;
+    }
+
+    return origAllowAction(element);
+  };
+
+  // NOTE: remote: true でファイルアップロードも有効に
+  const handleRemote = (form) => {
+    const formData = new FormData(form);
+    // NOTE: IEのバグ対策
+    // https://blog.yorkxin.org/2014/02/06/ajax-with-formdata-is-broken-on-ie10-ie11
+    formData.append('_file_upload', 1);
+
+    // NOTE: jquery-ujs で13ミリ秒後に無効→有効となった後に再度無効化
+    // https://github.com/rails/jquery-ujs/blob/3bff9e049afcb38191c6eb470062b9e51f46e535/src/rails.js#L507
+    setTimeout(() => {
+      $.rails.disableFormElements($(form));
+    }, 20);
+
+    $.ajax({
+      type: form.method || 'POST',
+      url: form.action,
+      data: formData,
+      processData: false,
+      contentType: false,
+    })
+      .done((...args) => {
+        $(form).trigger('ajax:success', args);
+      })
+      .fail((...args) => {
+        $(form).trigger('ajax:error', args);
+      })
+      .always((...args) => {
+        $(form).trigger('ajax:complete', args);
+      });
+  };
+
+  $(document).on('ajax:aborted:file', 'form', ({ currentTarget }) => {
+    handleRemote(currentTarget);
+    return false;
+  });
+}(jQuery));
