00064d55a81d9695aebf8ee7e658c59677ee30ce
aki <lala.akira@gmail.com>
2019-09-16 08:45:46 +0900

    Change to build with typescript

--- src/rails.ts
+++ src/rails.ts
@@ -1,4 +1,7 @@
-// eslint-disable-next-line func-names
+interface JQueryStatic {
+  rails: any
+}
+
 (function ($) {
   const { allowAction } = $.rails;
   const origAllowAction = allowAction.bind($.rails);
@@ -6,9 +9,9 @@
   // NOTE: HTML5のvalidation でエラーのある場合に confirm ダイアログを表示するとネイティブのエラーメッセージが表示されない動作の改善
   // https://github.com/rails/jquery-ujs/blob/3bff9e049afcb38191c6eb470062b9e51f46e535/src/rails.js#L294
   // eslint-disable-next-line no-param-reassign
-  $.rails.allowAction = (element) => {
+  $.rails.allowAction = (element: JQuery) => {
     if ($(element).is('input[type="submit"],button[type="submit"]')) {
-      const form = element.closest('form').get(0);
+      const form = element.closest('form').get(0) as HTMLFormElement;
       if (form && form.checkValidity && !form.checkValidity()) {
         return true;
       }
@@ -18,11 +21,11 @@
   };
 
   // NOTE: remote: true でファイルアップロードも有効に
-  const handleRemote = (form) => {
+  const handleRemote = (form: HTMLFormElement) => {
     const formData = new FormData(form);
     // NOTE: IEのバグ対策
     // https://blog.yorkxin.org/2014/02/06/ajax-with-formdata-is-broken-on-ie10-ie11
-    formData.append('_file_upload', 1);
+    formData.append('_file_upload', '1');
 
     // NOTE: jquery-ujs で13ミリ秒後に無効→有効となった後に再度無効化
     // https://github.com/rails/jquery-ujs/blob/3bff9e049afcb38191c6eb470062b9e51f46e535/src/rails.js#L507
@@ -37,18 +40,18 @@
       processData: false,
       contentType: false,
     })
-      .done((...args) => {
+      .done((...args: any) => {
         $(form).trigger('ajax:success', args);
       })
-      .fail((...args) => {
+      .fail((...args: any) => {
         $(form).trigger('ajax:error', args);
       })
-      .always((...args) => {
+      .always((...args: any) => {
         $(form).trigger('ajax:complete', args);
       });
   };
 
-  $(document).on('ajax:aborted:file', 'form', ({ currentTarget }) => {
+  $(document).on('ajax:aborted:file', 'form', ({ currentTarget }: { currentTarget: HTMLFormElement}) => {
     handleRemote(currentTarget);
     return false;
   });
